46a7f6f44ae3615820e5574bb9214710b220b8d5
Hemant Verma <41631241+HemantVermaDU@users.noreply.github.com>
2022-10-10 09:27:47 +0530

    Add files via upload

---
+++ food_Website_Responsive/script.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener("DOMContentLoaded",()=>{
+	function counter(id ,start ,end,duration){
+		let obj =document.getElementById(id),
+		current =start,
+		range =end-start,
+		increment = end > start?1:-1,
+		step = Math.abs(Math.floor(duration/range)), 
+		timer = setInterval(()=>{
+			current += increment;
+			obj.textContent = current;
+			if(current == end){
+				clearInterval(timer);
+			}
+		},step);
+	}
+	counter("count1",0,500,1000);
+	counter("count2",200,1000,1000);
+	counter("count3",0,800,1000);
+	counter("count4",0,200,1000);
+});
